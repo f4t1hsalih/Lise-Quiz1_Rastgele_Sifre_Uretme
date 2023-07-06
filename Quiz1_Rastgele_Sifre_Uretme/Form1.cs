fd14c14a3683085d584c3ee16f8d2fe5e0b9a8bb
f4t1hsalih <f4t1hsalih@gmail.com>
2023-07-06 18:53:53 +0300

    Add project files.

---
+++ Quiz1_Rastgele_Sifre_Uretme/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Quiz1_Rastgele_Sifre_Uretme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int a, b;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                a = rnd.Next(1, 9);
+                textBox1.Text += a.ToString();
+                b = rnd.Next(1, 6);
+                if (b == 1)
+                {
+                    textBox1.Text += "a";
+                }
+                else if (b == 2)
+                {
+                    textBox1.Text += "b";
+                }
+                else if (b == 3)
+                {
+                    textBox1.Text += "c";
+                }
+                else if (b == 4)
+                {
+                    textBox1.Text += "d";
+                }
+                else if (b == 5)
+                {
+                    textBox1.Text += "e";
+                }
+            }
+            a = rnd.Next(1, 9);
+            textBox1.Text += a.ToString();
+        }
+    }
+}
---
+++ Quiz1_Rastgele_Sifre_Uretme/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Quiz1_Rastgele_Sifre_Uretme
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
