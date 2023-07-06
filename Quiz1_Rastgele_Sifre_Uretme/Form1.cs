a0390f7fbb10705bc4375b4980a8df81814a76d3
f4t1hsalih <f4t1hsalih@gmail.com>
2023-07-06 18:56:15 +0300

    Kod kısaltması yapıldı

--- Quiz1_Rastgele_Sifre_Uretme/Form1.cs
+++ Quiz1_Rastgele_Sifre_Uretme/Form1.cs
@@ -11,6 +11,7 @@ namespace Quiz1_Rastgele_Sifre_Uretme
         }
 
         int a, b;
+
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
@@ -21,26 +22,17 @@ namespace Quiz1_Rastgele_Sifre_Uretme
                 textBox1.Text += a.ToString();
                 b = rnd.Next(1, 6);
                 if (b == 1)
-                {
                     textBox1.Text += "a";
-                }
                 else if (b == 2)
-                {
                     textBox1.Text += "b";
-                }
                 else if (b == 3)
-                {
                     textBox1.Text += "c";
-                }
                 else if (b == 4)
-                {
                     textBox1.Text += "d";
-                }
                 else if (b == 5)
-                {
                     textBox1.Text += "e";
-                }
             }
+
             a = rnd.Next(1, 9);
             textBox1.Text += a.ToString();
         }
